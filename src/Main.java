a355580daf6ed343a0dae28c5aa26d242da4ac46
zwer9 <ktlusty9@gmail.com>
2024-04-08 19:24:16 +0200

    Operatory

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        a += b;
+        a = a +b;
+        a /= b;
+        a = a /b;
+        a %= b;
+        a++;
+        a = a;
+        ++a;
+        a = a + 1;
+        System.out.printf("a%%b   %c + %c", 65, 97);
+    }
+}
